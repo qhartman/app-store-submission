23a5d39f5a2d95810f0691c74c954e59593dbbe8
Quentin Hartman <q@hartkraft.com>
2024-09-25 08:43:02 -0600

    simplfying request more

--- app_store_submit.py
+++ app_store_submit.py
@@ -56,7 +56,7 @@ def make_app_store_api_request(endpoint, method="GET", json_data=None):
     return response.json()
 
 def get_latest_app_store_build():
-    builds = make_app_store_api_request(f"/apps/{app_store_app_id}/builds?limit=1&fields=buildBundles")
+    builds = make_app_store_api_request(f"/apps/{app_store_app_id}/builds?limit=1")
     build = builds['data'][0]
     build_id = build['id']
     version_string = build['attributes']['version']
